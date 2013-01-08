65fb5c91467a509611d07735e0b323f8484991a3
Juan Manuel Tastzian <jm@tast.com.ar>
2013-01-08 16:31:04 -0300

    refactor problem07

--- src/problem07.rb
+++ src/problem07.rb
@@ -3,32 +3,14 @@
 # 
 # What is the 10 001st prime number?
 
+require "is_prime.rb"
+index = 2
+prime = 3
+current = prime
 
-def is_prime (prime_list,num)
-	return false if num%2 == 0
-	i = 1
-	prime = prime_list[i]
-	is_prime = true
-	num_sqrt = Math.sqrt(num)
-	while prime <= num_sqrt
-		if num%prime == 0
-			is_prime = false
-			return is_prime
-		end
-		i+=1
-		prime = prime_list[i]
-	end
-	return is_prime
+until index >= 10001 do
+	current += 2
+	(prime = current) && (index += 1) if current.prime?
 end
 
-prime_list = []
-prime_list.push(2)
-prime_list.push(3)
-current = prime_list[prime_list.length-1]+2
-begin
-	if is_prime(prime_list,current)
-		prime_list.push(current)
-	end
-	current+=2
-end while prime_list.length < 10001
-puts prime_list[prime_list.length-1]
+puts prime
