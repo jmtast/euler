fd41ede843df06ef19eea621e4d86216b9665e61
Juan Manuel Tastzian <jm@tast.com.ar>
2013-01-11 15:42:11 -0300

    problem15 green

--- src/problem15.rb
+++ src/problem15.rb
@@ -6,10 +6,21 @@
 # To calculate the ways to reach some position (i,j) in a nxn matrix, you can sum the ways to reach the position (i-1,j) and the position (i,j-1) recursively.
 
 def grid_routes(i,j)
-  routes = 2 if i == 1 && j == 1
-  routes += grid_routes(i-1,j) + grid_routes(i,j-1) if i > 1 && j > 1
-  routes += grid_routes(i-1,j) if j > 1
-  routes += grid_routes(i,j-1) if i > 1
+  routes = 0
+  return routes = 1 if i == 0 || j == 0
+  return routes = 2 if i == 1 && j == 1
+  return routes += grid_routes(i-1,j) + grid_routes(i,j-1) if i > 1 && j > 1
+  return routes += grid_routes(i-1,j) if i > 1 && j == 1
+  return routes += grid_routes(i,j-1) if j > 1 && i == 1
 end
 
-puts grid_routes(20,20)
+size = 20
+puts grid_routes(size,size)/2
+
+
+
+# 1  1  1  1  1
+# 1  2  3  4  5
+# 1  3  6  10 15
+# 1  4  10 20 35
+# 1  5  15 35 70
