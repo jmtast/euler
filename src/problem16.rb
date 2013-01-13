dc5ee7f80568127d1465128665bca5451b52d16b
Juan Manuel Tastzian <jm@tast.com.ar>
2013-01-13 17:18:56 -0300

    problem16 green

---
+++ src/problem16.rb
@@ -0,0 +1,12 @@
+# PROBLEM 16:
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+# What is the sum of the digits of the number 2^1000?
+
+number = 2
+
+for i in 1...1000
+	number *= 2
+end
+
+puts number.to_s.split("").map(&:to_i).reduce(:+)
