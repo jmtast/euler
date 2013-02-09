ededb966b99b71f4a44e523deeb638162b837f7a
Juan Manuel Tastzian <jm@tast.com.ar>
2013-02-08 23:55:56 -0300

    problem18 refactored and problem67 green

--- src/problem67.rb
+++ src/problem67.rb
@@ -1,4 +1,4 @@
-# PROBLEM 17:
+# PROBLEM 67:
 # By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
 
 # 3
@@ -8,26 +8,10 @@
 
 # That is, 3 + 7 + 4 + 9 = 23.
 
-# Find the maximum total from top to bottom of the triangle below:
+# Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.
 
-# 75
-# 95 64
-# 17 47 82
-# 18 35 87 10
-# 20 04 82 47 65
-# 19 01 23 75 03 34
-# 88 02 77 73 07 63 67
-# 99 65 04 28 06 16 70 92
-# 41 41 26 56 83 40 80 70 33
-# 41 48 72 33 47 32 37 16 94 29
-# 53 71 44 65 25 43 91 52 97 51 14
-# 70 11 33 28 77 73 17 78 39 68 17 57
-# 91 71 52 38 17 14 91 43 58 50 27 29 48
-# 63 66 04 68 89 53 67 30 73 16 69 87 40 31
-# 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
-
-# NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge
-# with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
+# NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether! If you could
+# check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
 
 class Element
   def initialize(num)
@@ -52,21 +36,10 @@ class Element
   end
 end
 
-class Array
-  def printm
-    each do |row|
-      row.each do |element|
-        print "#{element.best_sum} "
-      end
-      print "\n"
-    end
-  end
-end
-
 #### This block of code is to build the matrix from file ####
 
 matrix = []
-File.open("../lib/triangle_input.txt", "r").each_line.with_index do |line, i|
+File.open("../lib/triangle.txt", "r").each_line.with_index do |line, i|
   line.split(" ").each.with_index do |num, j|
     elem = Element.new(num)
     if j == 0
@@ -102,7 +75,7 @@ end
 max = 0
 matrix.each_with_index do |row, i|
   row.each_with_index do |element, j|
-    if i + j == 14 and matrix[i][j].best_sum > max
+    if i + j == 99 and matrix[i][j].best_sum > max
       max = matrix[i][j].best_sum
     end
   end
